993475efbcaf44e2837cd8bd5fbc82ee1122879d
arzynik <github@arzynik.com>
2012-11-27 10:24:14 -0800

    some cleanup

--- include/controllers/default/sample/api/sampleobject/index.php
+++ include/controllers/default/sample/api/sampleobject/index.php
@@ -4,24 +4,16 @@ class Controller_api_sampleobject extends Sample_Controller_Rest {
 	public function init() {
 		switch ($this->method()) {
 			case 'post':
-				// do stuff for posting
-				if ($_SESSION['admin']) {
-					// save the restaurant
-					$o = SampleObject::o(c::getPagePiece(2));
+				// save the object
+				$o = SampleObject::o(c::getPagePiece(2));
+				$o->serialize($this->request());
+				$o->save();
+				echo $o->json();
 
-					switch (c::getPagePiece(3)) {
-						default:
-							$request = $this->request();
-							$o->serialize($request);
-							$o->save();
-							echo $o->json();
-							break;
-					}
-					
-				}
 				break;
 
 			case 'get':
+				// output the object
 				$out = SampleObject::o(c::getPagePiece(2));
 				if ($out->id_sampleobject) {
 					echo $out->json();
